19dfa0cfd5e0c9e89e82abc721ace35e4c963986
Cristian Gabriel Huichaqueo <cristianhuichaqueo@gmail.com>
2015-10-01 11:21:55 -0300

    Se quitan los metodos porque ya estan en la clase abstracta

--- src/main/java/com/group7/dao/ClienteDAO.java
+++ src/main/java/com/group7/dao/ClienteDAO.java
@@ -2,62 +2,15 @@ package com.group7.dao;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import com.group7.entity.Cliente;
 
-import util.HibernateUtil;
-
 public class ClienteDAO extends AbstractDAO<Cliente> implements DaoInterface<Cliente, Integer> {
-
-	private Session currentSession;
-	private Transaction currentTransaction;
-
-	public Session openCurrentSession() {
-		currentSession = getSessionFactory().openSession();
-		return currentSession;
-	}
-
-	public Session openCurrentSessionwithTransaction() {
-		currentSession = getSessionFactory().openSession();
-		currentTransaction = currentSession.beginTransaction();
-		return currentSession;
-	}
-
-	public void closeCurrentSession() {
-		currentSession.close();
-	}
-
-	public void closeCurrentSessionwithTransaction() {
-		currentTransaction.commit();
-		currentSession.close();
-	}
-
-	private static SessionFactory getSessionFactory() {
-		return HibernateUtil.getSessionFactory();
-	}
-
-	public Session getCurrentSession() {
-		return currentSession;
-	}
-
-	public void setCurrentSession(Session currentSession) {
-		this.currentSession = currentSession;
-	}
-
-	public Transaction getCurrentTransaction() {
-		return currentTransaction;
-	}
-
-	public void setCurrentTransaction(Transaction currentTransaction) {
-		this.currentTransaction = currentTransaction;
-	}
 
 	public void agregarOficina(Cliente c) {
 		Transaction t = getCurrentSession().beginTransaction();
-		String sql1 = " UPDATE Cliente SET ODV = ? Where CUILCliente = ?";
+		String sql1 = " UPDATE Cliente SET ODV = ? WHERE CUILCliente = ?";
 		getCurrentSession().createQuery(sql1).setInteger(0, c.getOficinaVentas().getIdOficinaVenta()).setInteger(1, c.getcUILCliente()).executeUpdate();
 		t.commit();
 	}
@@ -85,7 +38,7 @@ public class ClienteDAO extends AbstractDAO<Cliente> implements DaoInterface<Cli
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Cliente> buscarTodos() {
-		return (List<Cliente>)getCurrentSession().createQuery("from Cliente Where estado = 1").list();
+		return (List<Cliente>)getCurrentSession().createQuery("FROM Cliente WHERE estado = 1").list();
 	}
 
 	@Override
